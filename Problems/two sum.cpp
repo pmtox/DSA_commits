d41bfde44721175ca70ce06b1f883de3a3a25818
Prashant Mishra <rishi24092006@gmail.com>
2025-08-15 03:20:08 +0530

    Create two sum.cpp

---
+++ Problems/two sum.cpp
@@ -0,0 +1,39 @@
+//TWO SUM PROBLEM
+//AUTHOR :: PRASHANT MISHRA
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+//above class was written at @leetcode 
+
+int main() {
+    vector<int> nums = {8, 6, 19, 15};
+    int target = 25;
+
+    Solution obj;
+    vector<int> result = obj.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << " and " << result[1] << endl;
+        cout << "Values: " << nums[result[0]] << " and " << nums[result[1]] << endl;
+    } else {
+        cout << "No two numbers sum up to the target." << endl;
+    }
+
+    return 0;
+}
