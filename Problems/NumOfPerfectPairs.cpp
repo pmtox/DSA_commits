293759b395aa9a86a37504fbf0a5eed5b16ba231
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-08-17 01:43:56 +0530

    Create NumOfPerfectPairs.cpp

---
+++ Problems/NumOfPerfectPairs.cpp
@@ -0,0 +1,21 @@
+//PROBLEM OF 
+//NUMBER OF PERFECT PAIRS
+//NAME : PRASHANT MISHRA
+
+class Solution {
+public:
+    long long perfectPairs(vector<int>& nums) {
+        int n = nums.size();
+        long long count = 0;
+        for (int i=0; i <n ; i++){
+            for (int j = i+1 ; j< n; j++){
+                long long a = nums[i] , b = nums[j];
+                long long left = min(abs(a-b) , abs(a+b));
+                long long right = min(abs(a) , abs(b));
+                if (left <= right){
+                    count++;
+                }
+            }
+        } return count;
+    }
+};
