724260e3204b25e039f245aa0e0b0b88bfe201e8
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-08-20 22:44:18 +0530

    Update ChefAndcandies.cpp

--- CodeChef500-800/ChefAndcandies.cpp
+++ CodeChef500-800/ChefAndcandies.cpp
@@ -13,9 +13,9 @@ int main(){
         int N, X;
         cin >> N >> X;
         if (X >= N) cout << 0 << '\n';
-        else cout <<((N - X+3) / 4) << '\n';
+        else cout <<((N - X+3) / 4) << '\n';  //WE CAN USE ceil() to round up the result.
     }
     return 0;
 }
 
-//
+//THis is a 570 difficulty problem on 500 to 800 difficulty rating practice track on CODECHEF.
