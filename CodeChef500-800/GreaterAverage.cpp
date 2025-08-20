498288650d4ed8a1926302c05b8901459f302ff0
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-08-21 02:10:43 +0530

    Create GreaterAverage.cpp

---
+++ CodeChef500-800/GreaterAverage.cpp
@@ -0,0 +1,23 @@
+//Greater Average PROBLEM
+//NAME: PRASHANT MISHRA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	float A , B , C;
+	while(t--){
+	    cin >> A >> B>>C;
+	    float avgg=(A+B)/2;
+	    if(avgg>C){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	} return 0;
+}
+
+
+//USED IF ELSE AND JUST AVERAGE FORMULA
