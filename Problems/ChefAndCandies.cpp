7d9cede08fdddf6587a0999c30c3a18128109803
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-08-20 22:24:27 +0530

    Create ChefAndCandies.cpp

---
+++ Problems/ChefAndCandies.cpp
@@ -0,0 +1,19 @@
+//CHEF AND CANDIES PROBLEM-CODECHEF
+//NAME : PRASHANT MISHRA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int t; 
+    cin >> t;
+    while(t--){
+        int N, X;
+        cin >> N >> X;
+        if (X >= N) cout << 0 << '\n';
+        else cout <<((N - X+3) / 4) << '\n';
+    }
+    return 0;
+}
+
