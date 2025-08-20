eff6bf9973609ed9fadb16c2c330bbcacdbef60e
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-08-20 22:48:58 +0530

    Create ExpertSetter.cpp

---
+++ CodeChef500-800/ExpertSetter.cpp
@@ -0,0 +1,19 @@
+//Expert Setter PROBLEM
+//NAME : PRASHANT MISHRA
+//DIFFICULTY : 561
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	double a, b;
+	while(t--){
+	    cin>>a>>b;
+	    cout<<(2*b>=a?"YES\n":"NO\n");
+	}
+	return 0;
+}
+
+//This is the EXPERT SETTER PRACTICE QUESTION ON CODECHEF 
