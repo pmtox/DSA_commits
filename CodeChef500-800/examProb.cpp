c45fb58f94f287c2f61621fea26a3638bb2e124d
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-08-22 02:54:47 +0530

    Create examProb.cpp

---
+++ CodeChef500-800/examProb.cpp
@@ -0,0 +1,23 @@
+//EXAM PROBLEM
+//NAME: PRASHANT MISHRA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	int X, Y , Z;
+	while(t--){
+	    cin>>X>>Y>>Z;
+	    if(Z>X*Y/2){
+	        cout<<"YES"<<endl;
+	    } else{
+	        cout<<"NO"<<endl;
+	    }
+	}return 0;
+}
+
+//WE DID IT USING SIMPLE MATHS.
+
+
