a49f6277a80008a289c75dae73eb8b053eb5ac51
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-08-23 22:38:20 +0530

    Create rvrseInt.cpp

---
+++ CodeChef500-800/rvrseInt.cpp
@@ -0,0 +1,25 @@
+//REVERSE INTEGER PROBLEM
+//NAME: PRASHANT MISHRA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;cin>>t;
+    int a;
+    while(t--){
+        cin>>a;
+        int reverse = 0;
+        while(a>0){
+            int dig = a%10;
+            reverse = reverse*10+dig;
+            a = a/10;
+ 
+        }
+        cout<<reverse<<endl;
+    }
+}
+
+
+//WE USED SIMPLE MATHEMATICAL OPERATIONS IN THIS.
+// WE CAN ALSO USE string in this to reverse using reverse();
