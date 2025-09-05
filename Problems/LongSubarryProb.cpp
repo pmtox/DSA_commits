530c7802391a2af0b63e84fce2ae1a980c5e8a9b
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-09-06 00:00:04 +0530

    Update LongSubarryProb.cpp

--- Problems/LongSubarryProb.cpp
+++ Problems/LongSubarryProb.cpp
@@ -1,1 +1,15 @@
 
+int longestSub(vector<int> &arr , int k){
+  long long maxLen = 0; int n = arr.size();
+  long long right = 0 , left = 0;
+  long long sum = arr[0];
+  while(right<n){
+    while(left <= right && sum >k){
+      sum -=arr[left];
+      left++;
+    } if(sum==k){
+      maxLen = max(maxLen , right-left +1);
+    } right ++;
+    if(right<n) sum+= arr[right];
+  } return maxLen;
+}
