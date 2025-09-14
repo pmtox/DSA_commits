d23ac74dea1159bcf913a29d65edc07915580ec9
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-09-14 23:15:10 +0530

    Create ChefandNextGen.cpp

---
+++ CodeChef500-800/ChefandNextGen.cpp
@@ -0,0 +1,20 @@
+//Chef and NextGen PROBLEM
+//NAME: PRASHANT MISHRA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	int arry[t];
+	for(int i = 0; i< t ; i++){
+	    cin>>arry[i] >>arry[i+1] >>arry[i+2]>>arry[i+3];
+	    if(arry[i]*arry[i+1] <= arry[i+2]*arry[i+3]){
+	        cout<<"YES" <<endl;
+	    } else{
+	        cout<<"NO" << endl;
+	    }
+	} return 0;
+}
+
