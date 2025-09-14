5b89ac5e0c63efc2b1e3307b639646ee23f6e930
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-09-14 23:13:14 +0530

    Create CheflandGames.cpp

---
+++ CodeChef500-800/CheflandGames.cpp
@@ -0,0 +1,19 @@
+//Chefland Games PROBLEM
+//Name: PRASHANT MISHRA
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	int a,b,c,d;
+    while(t--){
+        cin>>a>>b>>c>>d;
+        if(a+b+c+d >= 1){
+            cout<<"OUT"<<endl;
+        } else{
+            cout<<"IN"<<endl;
+        }
+    }
+}
