10c6c6ed890a9337c3042586ae02fd96c5e65dc7
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-09-14 23:16:07 +0530

    Create SugarcaneJuiceBusiness.cpp

---
+++ CodeChef500-800/SugarcaneJuiceBusiness.cpp
@@ -0,0 +1,16 @@
+//Sugarcane Juice Business PROBLEM
+//NAME: PRASHANT MISHRA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	int a;
+	for(int i =0; i<t;i++){
+	    cin>>a;
+	    double tot = a*50;
+	    long long k = tot - (tot*70/100);
+	    cout<< k <<endl;
+	}
+}
