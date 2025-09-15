07b6b36f9ad487aa56f086a4642a474d3c416bb7
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-09-15 23:27:22 +0530

    Create CounttheNotebooks.cpp

---
+++ CodeChef500-800/CounttheNotebooks.cpp
@@ -0,0 +1,16 @@
+//Count the Notebooks PROBLEM
+//NAME: PRASHANT MISHRA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	int n;
+	while(t--){
+	    cin>>n;
+	    int ntbs = 10*n;
+	    cout<<ntbs << endl;
+	} return 0;
+}
