496939aa3d949a7be20642d933b9b83c01ac3eb0
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-09-27 03:15:06 +0530

    Create 2149A.cpp

---
+++ CF/2149A.cpp
@@ -0,0 +1,35 @@
+//A. Be Positive
+//NAME: PRASHANT MISHRA
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void chk(vector<int>& nums){
+    int count0 = 0, countNeg = 0;
+    for (int x : nums) {
+        if (x == 0) count0++;
+        else if (x == -1) countNeg++;
+    }
+    long long ans = count0;
+    if (countNeg % 2 != 0) ans += 2;
+    cout << ans << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+int t;cin>>t;
+int a; 
+while(t--){
+    cin>>a;
+    vector<int>nums;
+    for(int i =0;i<a;i++){
+        int x; cin>>x;
+        nums.push_back(x);
+        }
+    chk(nums);
+    }
+}
