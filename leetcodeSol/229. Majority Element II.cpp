4e45204ce95f97e0492428a96c2343779f64b7d1
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-10-19 09:18:17 +0530

    Update 229. Majority Element II.cpp

--- leetcodeSol/229. Majority Element II.cpp
+++ leetcodeSol/229. Majority Element II.cpp
@@ -30,3 +30,27 @@ public:
         return kk;
     }
 };
+
+
+//_____________________________________________________________________________________________________________________________________________________
+
+
+//APPROACH 2:
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ls;
+        map<int , int> mpp;
+        int n = nums.size();
+        int mm = (int) (n/3) + 1;
+        for(int i =0 ; i<n;i++){
+            mpp[nums[i]]++;
+            if(mpp[nums[i]] == mm){
+                ls.push_back(nums[i]);
+            }
+            if(ls.size() == 2) break;
+        }
+        return ls;
+    }
+};
