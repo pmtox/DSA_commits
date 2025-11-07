5a1c87dd4f88134cf52c5c43afdce790bd5d6f38
Prashant Mishra <180631467+pmtox@users.noreply.github.com>
2025-11-07 23:21:11 +0530

    Create 121. Best Time to Buy and Sell Stock.cpp

---
+++ leetcodeSol/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,18 @@
+//121. Best Time to Buy and Sell Stock
+//Leetcode Easy
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mxProf =0;
+        int mini = prices[0];
+        int n = prices.size();
+        for(int i = 0; i<n ; i++){
+            int cost = prices[i] -mini;
+            mxProf = max(mxProf , cost);
+            mini = min(mini , prices[i]);
+        }
+        return mxProf;
+    }
+};
