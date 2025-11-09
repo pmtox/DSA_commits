00c4808666aba0983c6463100b825d601e06803a
Prashant Mishra <rishi24092006@gmail.com>
2025-11-09 23:46:55 +0530

    Update 3289. The Two Sneaky Numbers of Digitville.cpp

--- leetcodeSol/3289. The Two Sneaky Numbers of Digitville.cpp
+++ leetcodeSol/3289. The Two Sneaky Numbers of Digitville.cpp
@@ -17,3 +17,21 @@ public:
         return ans;
     }
 };
+
+
+_________________approach II_________________________
+
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> ans;
+
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] == nums[i-1]) {
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
