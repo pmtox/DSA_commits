94f248859a6602dc7a614c8c7c1ce0c78908f570
pmtox <pmishraa24@gmail.com>
2025-12-31 01:29:01 +0530

    updated 73. Set Matrix Zeroes

--- Array/73. Set Matrix Zeroes.cpp
+++ Array/73. Set Matrix Zeroes.cpp
@@ -1,7 +1,42 @@
 //73. Set Matrix Zeroes
 //Leetcode Easy
 
+// T.C. O((N*M) * (N+M))
+// S.C. O(N*M)
 
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> vis = matrix;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(matrix[i][j] == 0){
+                    for(int k = 0; k < m ; k++){   
+                        //move across all columns of the specific row;
+                        //and making em' zero(0);
+                        vis[i][k] = 0;                 
+                    }
+                }
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j< m ; j++){
+                if(matrix[i][j]==0){
+                    for(int k = 0 ; k< n ; k++){
+                        vis[k][j]=0;
+                    }
+                }
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                matrix[i][j] = vis[i][j];
+            }
+        }
+    }
+};
 
 // T.C. -> O(N*M)
 // S.C. -> O(N+M)
@@ -30,3 +65,8 @@ public:
         }
     }
 };
+
+// T.C. O(2*(N*M))
+// S.C. O(1)
+
+
