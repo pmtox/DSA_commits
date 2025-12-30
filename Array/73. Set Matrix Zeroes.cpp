399c8b98409e530f97722111f978a6b603428668
pmtox <pmishraa24@gmail.com>
2025-12-31 02:00:54 +0530

    updated 73. Set Matrix Zeroes

--- Array/73. Set Matrix Zeroes.cpp
+++ Array/73. Set Matrix Zeroes.cpp
@@ -1,6 +1,9 @@
 //73. Set Matrix Zeroes
 //Leetcode Easy
 
+
+//--------------------------------APPROACH-I------------------------------------------//
+
 // T.C. O((N*M) * (N+M))
 // S.C. O(N*M)
 
@@ -38,6 +41,8 @@ public:
     }
 };
 
+
+//--------------------------------APPROACH-II------------------------------------------//
 
 // T.C. -> O((N*M) * (N+M))
 // S.C. -> O(1)
@@ -80,7 +85,7 @@ public:
 };
 
 
-
+//--------------------------------APPROACH-III------------------------------------------//
 
 // T.C. -> O(N*M)
 // S.C. -> O(N+M)
@@ -110,7 +115,50 @@ public:
     }
 };
 
+
+//--------------------------------APPROACH-IV------------------------------------------//
+
 // T.C. O(2*(N*M))
 // S.C. O(1)
 
-
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        // rows = matrix[i][0];
+        // columns = matrix[0][j];
+        int cnt = 1;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    if(j != 0){
+                        matrix[0][j]= 0;
+                    } else{
+                        cnt = 0;
+                    }
+                }
+            }
+        }
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 1; j< m ; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int j = 0; j< m ; j++){
+                matrix[0][j] =0;
+            }
+        }
+        if(cnt == 0){
+            for(int i = 0; i < n ; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
