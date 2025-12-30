e93faffd341678fbe9798321f0f5dd1743b818e4
pmtox <pmishraa24@gmail.com>
2025-12-31 01:36:58 +0530

    Updated 73. Set Matrix Zeroes

--- Array/73. Set Matrix Zeroes.cpp
+++ Array/73. Set Matrix Zeroes.cpp
@@ -38,6 +38,50 @@ public:
     }
 };
 
+
+// T.C. -> O((N*M) * (N+M))
+// S.C. -> O(1)
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<m; k++){
+                        if(matrix[i][k] != 0){
+                            matrix[i][k] = -9999;
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<n; k++){
+                        if(matrix[k][j] != 0){
+                            matrix[k][j] = -9999;
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == -9999){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+
+
+
 // T.C. -> O(N*M)
 // S.C. -> O(N+M)
 
