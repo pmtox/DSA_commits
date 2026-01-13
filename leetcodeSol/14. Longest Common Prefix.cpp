c1c69ab6ada8cdc4bf19d35452b2b5b0a55b6ee2
pmtox <pmishraa24@gmail.com>
2026-01-13 14:42:57 +0530

    longest common prefix

---
+++ leetcodeSol/14. Longest Common Prefix.cpp
@@ -0,0 +1,17 @@
+// 14. Longest Common Prefix
+// LEETCODE EASY
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        for(int i =0; i< strs[0].size() ; i++){
+            char ch = strs[0][i];
+            for(int j = 0; j< strs.size(); j++){
+                if(i >= strs[j].size() || ch != strs[j][i]){
+                    return strs[0].substr(0 , i);
+                }
+            }
+        }
+        return strs[0];
+    }
+};
