7605a1fd6e6d4c9082ed5605859083ed70af1cb9
pmtox <pmishraa24@gmail.com>
2026-01-14 17:24:23 +0530

    design hashmap problem

---
+++ leetcodeSol/706. Design HashMap.cpp
@@ -0,0 +1,22 @@
+// 706. Design HashMap
+// LEETCODE EASY
+
+class MyHashMap {
+private:
+    vector<int> data;
+public:
+    MyHashMap(): data(1000001 , -1){
+    }
+    
+    void put(int key, int value) {
+        data[key] = value;
+    }
+    
+    int get(int key) {
+        return data[key];
+    }
+    
+    void remove(int key) {
+        data[key] = -1;
+    }
+};
